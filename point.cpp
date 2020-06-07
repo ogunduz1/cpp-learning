d31a5a88f0b4894fae5ef01225c69d0572ad90a0
ozgurgunduz <ozgurgnduz@gmail.com>
2020-06-08 00:05:20 +0300

    out scope func.

--- point.cpp
+++ point.cpp
@@ -3,14 +3,28 @@
 using namespace std;
 
 class Nokta{
-public:
+private:
 	int x,y;
-	void addValue(int,int);
+
+//protected:kalıtımda kullanılır, dışarıya kapalıdır
+public:
+	int getX();
+	int getY();
+	void setX(int);
+	void setY(int);
 	void display();
 	bool IsOrigin();
 };
-void Nokta::addValue(int _x, int _y){
+int Nokta::getX(){
+	return x;
+}
+int Nokta::getY(){
+	return y;
+}
+void Nokta::setX(int _x){
 	x=_x;
+}
+void Nokta::setY(int _y){
 	y=_y;
 }
 void Nokta::display(){
@@ -22,31 +36,9 @@ bool Nokta::IsOrigin(){
 
 int main(){
 	Nokta n1;
-	Nokta n2;
-	Nokta* n3;
-	Nokta* n4;
-	n4=new Nokta();
-	n3=&n2;
-	Nokta array[10];
-	Nokta* pArray;
-	pArray=new Nokta[10];
-	n1.addValue(4,6);
-	n2.addValue(3,7);
-	//cout<<n1.x<<" , "<<n1.y<<endl;
-	n1.display();
-	//n2.display();
-	cout<<"Is it on origin? 0 No 1 Yes : "<<n1.IsOrigin()<<endl;
-	cout<<"Is it on origin? 0 No 1 Yes : "<<n1.IsOrigin()<<endl;
-	n3->display();
-	n4->addValue(55,90);
-	n4->display();
-	for(int i=0;i<10;i++){
-		array[i].addValue(i*2,i*2);
-		//pArray[i]->addValue(i*3,i*3); bu yanlış 
-		pArray[i].addValue(i*3,i*3);//alternatif olarak=> (pArray+sizeof(Nokta)+1)->addValue  
-		array[i].display();
-		pArray[i].display();
-	}
+	n1.setX(5);
+	n1.setY(12);
+	cout<<n1.getX()<<" , ";
+	cout<<n1.getY()<<endl;
 	return 0;
 }
-
